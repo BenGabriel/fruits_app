f6aa2073f291233dc3821a76b4a7e59d628e4b68
unknown <simeongabriel175@gmail.com>
2022-03-05 20:46:37 -0800

    first commit

---
+++ src/Data.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    id: 1,
+    image: '',
+    name: 'Eggplant',
+    type: 'Vegetable',
+    color: '',
+    variety:'Larger Eggplant:',
+    description:
+      'Eggplants are pear-shaped and have purple shiny skin. They are categorised by size. Casseroles, layered vegetable dishes, curries, baba ghanoush.',
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'Onion',
+    type: 'Vegetable',
+    color: '',
+    variety:"Brown Onion",
+    description:
+      'The onion, also known as the bulb onion or common onion, is a vegetable that is the most widely cultivated species of the genus Allium',
+  },
+  {
+    id: 3,
+    image: '',
+    name: 'Lettuce',
+    type: 'Vegetable',
+    color: '',
+    variety:'Icerberg',
+    description:
+      'Lettuce is an annual plant of the daisy family, Asteraceae. It is most often grown as a leaf vegetable, but sometimes for its stem and seeds',
+  },
+  {
+    id: 4,
+    image: '',
+    name: 'Orange',
+    type: 'Fruits',
+    color: '',
+    variety: 'Navel Orange',
+    description:
+      'An orange is a fruit of various citrus species in the family Rutaceae. it primarily refers to Citrus sinensis, which is also called sweet orange',
+  },
+  {
+    id: 5,
+    image: '',
+    name: 'Mango',
+    type: 'Fruits',
+    color: '',
+    variety: 'Delta Mango',
+    description:
+      'A mango is an edible stone fruit produced by the tropical tree Mangifera indica which is believed to have originated from the region between northwestern Myanmar, Bangladesh, and northeastern India',
+  },
+  {
+    id: 6,
+    image: '',
+    name: 'Cherry',
+    type: 'Fruits',
+    color: '',
+    variety:'Napoleon cherry',
+    description:
+      'A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit).',
+  },
+  {
+    id: 7,
+    image: '',
+    name: 'Bell Pepper',
+    type: 'Pepper',
+    color: '',
+    variety:'Sweet Pepper',
+    description:
+      'The bell pepper is the fruit of plants in the Grossum cultivar group of the species Capsicum annuum',
+  },
+  {
+    id: 8,
+    image: '',
+    name: 'Chilli Pepper',
+    type: 'Pepper',
+    color: '',
+    variety:'Chille Pepper',
+    description:
+      'Chili peppers, from Nahuatl chīlli, are varieties of the berry-fruit of plants from the genus Capsicum, which are members of the nightshade family Solanaceae, cultivated for their pungency',
+  },
+  {
+    id: 9,
+    image: '',
+    name: 'Cubanelle Pepper',
+    type: 'Pepper',
+    color: '',
+    variety:'Cuban Pepper',
+    description:
+      'The Cubanelle, is a variety of sweet pepper of the species Capsicum annuum.',
+  },
+];
